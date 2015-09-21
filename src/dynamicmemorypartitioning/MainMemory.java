0d594db493c72b53cf3fc14208c7f764ac8d340b
parias <parias@aggies.ncat.edu>
2015-09-21 16:39:17 -0400

    Adding implementations, Need to unallocate Memory Blocks

--- src/dynamicmemorypartitioning/MainMemory.java
+++ src/dynamicmemorypartitioning/MainMemory.java
@@ -11,6 +11,7 @@
  */
 package dynamicmemorypartitioning;
 
+import static java.lang.Math.abs;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Queue;
@@ -62,43 +63,101 @@ public class MainMemory {
 
     public void addBlockFirstFit(MemoryBlock block) {
 
-        for (int i = 0; i < list.size(); i++) {
-
+        for (int i = nextLocation; i < list.size(); i++) {
             if (queue.isEmpty()) {
                 if (!list.get(i).isOccupied() && (list.get(i).getBlockSize() > block.getBlockSize())) {
+                    MemoryBlock remainingBlock = new MemoryBlock(list.get(i).getBlockSize() - block.getBlockSize(),
+                            list.get(i).getRequestedTime(),
+                            false);
+                    list.add(i, block);
+                    list.set(i + 1, remainingBlock);
+                }
+            } else {
+                queue.add(block);
+            }
+        }
+
+    }
+
+    public void addBlockNextFit(MemoryBlock block) {
+
+        boolean foundFit = false;
+        outerLoop:
+        for (int i = nextLocation; i < list.size(); i++) {
+            if (queue.isEmpty()) {
+                if (!list.get(i).isOccupied() && block.getBlockSize() < list.get(i).getBlockSize()) {
+                    nextLocation = i;
                     MemoryBlock remainingBlock = new MemoryBlock(list.get(i).getBlockSize() - block.getBlockSize(),
                             list.get(i).getRequestedTime(),
                             false);
                     list.add(i, block);
                     list.set(i + 1, remainingBlock);
+                    foundFit = true;
+                    break outerLoop;
                 }
             } else {
                 queue.add(block);
             }
         }
+        if (foundFit == false) {
+            otherLoop:
+            for (int i = 0; i < nextLocation; i++) {
+                if (queue.isEmpty()) {
+                    if (!list.get(i).isOccupied() && block.getBlockSize() < list.get(i).getBlockSize()) {
+                        nextLocation = i;
+                        MemoryBlock remainingBlock = new MemoryBlock(list.get(i).getBlockSize() - block.getBlockSize(),
+                                list.get(i).getRequestedTime(),
+                                false);
+                        list.add(i, block);
+                        list.set(i + 1, remainingBlock);
+                        break otherLoop;
+                    } else {
+                        queue.add(block);
+                    }
+                }
+            }
+        }
+
     }
 
-    public void addBlockNextFit(MemoryBlock block) {
-       
-        for (int i = 0; i < list.size(); i++) {
+    public void addBlockBestFit(MemoryBlock block) {
+
+        if (checkFit(block)) {
             if (queue.isEmpty()) {
-                if (!list.get(i).isOccupied() && (list.get(i).getBlockSize() > block.getBlockSize())) {
-                    MemoryBlock remainingBlock = new MemoryBlock(list.get(i).getBlockSize() - block.getBlockSize(),
-                            list.get(i).getRequestedTime(),
-                            false);
-                    list.add(i, block);
-                    list.set(i + 1, remainingBlock);
+                int difference = Integer.MAX_VALUE;
+                int location = -1;
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.get(i).isOccupied() == false) {
+                        int localDiff = list.get(i).getBlockSize() - block.getBlockSize();
+                        if ((localDiff >= 0) && (abs(localDiff) < difference)) {
+                            difference = abs(localDiff);
+                            location = i;
+                        }
+                    }
                 }
+                MemoryBlock remainingBlock = new MemoryBlock(list.get(location).getBlockSize() - block.getBlockSize(),
+                        list.get(location).getRequestedTime(),
+                        false);
+                list.add(location, block);
+                list.set(location + 1, remainingBlock);
             } else {
                 queue.add(block);
             }
-
+        } else {
+            queue.add(block);
         }
 
     }
 
-    public void addBlockBestFit(int size) {
-
+    public boolean checkFit(MemoryBlock block) {
+        boolean fit = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).isOccupied() == false
+                    && block.getBlockSize() < list.get(i).getBlockSize()) {
+                fit = true;
+            }
+        }
+        return fit;
     }
 
 }
