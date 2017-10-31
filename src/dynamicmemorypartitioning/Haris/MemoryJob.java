dd0670670585aae462452c8d712212546e98c68c
user <user@10.211.17.165>
2017-10-31 10:21:41 +0800

    Functions added

--- src/dynamicmemorypartitioning/Haris/MemoryJob.java
+++ src/dynamicmemorypartitioning/Haris/MemoryJob.java
@@ -16,18 +16,28 @@ public class MemoryJob {
     private int arrivalTime;
     private int processTime;
     private int jobSize;
+    private boolean processStatus;
     
     public MemoryJob(){
     }
     
-    public MemoryJob(int jobNum, int arrivalTime, int processTime, int jobSize){
+    public MemoryJob(int jobNum, int arrivalTime, int processTime, int jobSize, boolean processing){
         this.jobNum = jobNum;
         this.arrivalTime = arrivalTime;
         this.processTime = processTime;
         this.jobSize = jobSize;
+        this.processStatus = processing;
     }
     
     //methods to store objects into the memory block list. since I can't figure out a way like c++;
+    public void setProcessStatus(boolean processing){
+        this.processStatus = processing;
+    }
+    
+    public boolean getProcessStatus(){
+        return this.processStatus;
+    }
+    
     public void setJobNum(int jobNum) {
         this.jobNum=jobNum;
     }
