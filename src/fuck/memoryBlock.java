cec8530559d8bd95e67659a4181baf4eb2ae9f5c
DonutRanger <korehadonut@gmail.com>
2017-11-01 13:51:44 +0800

    Starting another package

---
+++ src/fuck/memoryBlock.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fuck;
+
+/**
+ *
+ * @Iman Haris Bin Hadi DonutRanger
+ */
+public class memoryBlock {
+    int jobNumber;
+    int jobTime;
+    int jobTimeRemaining;
+    int mStartB;
+    int mLowB;
+    int memorySize;
+
+    String memoryStatus;
+}
